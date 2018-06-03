343a94d5fe8cf21e162393e2f9218d07a9df6fa4
Jon Allen <ylixir@gmail.com>
2018-06-03 03:07:35 -0500

    added gaming layer to the tomato

--- keyboards/tomato/keymaps/ylixir/keymap.c
+++ keyboards/tomato/keymaps/ylixir/keymap.c
@@ -1,5 +1,16 @@
 #include "tomato.h"
 
+enum layers {
+  DEFAULT,
+  NUM_FUNC,
+  SYMBOLS,
+  MOVEMENT,
+  BIG_MOVE,
+  RGB,
+  GAMING,
+  GAMING_EXTRA
+};
+
 #define SPC_F1 LT(1,KC_SPC)
 #define BS_F2 LT(2,KC_BSPC)
 #define ESC_F3 LT(3,KC_ESC)
@@ -12,7 +23,9 @@
 #define B_ALT MT(MOD_RALT, KC_B)
 #define M_GUI MT(MOD_RGUI, KC_M)
 #define ENT_SFT MT(MOD_RSFT, KC_ENT)
-
+#define TO_GAME TO(GAMING)
+#define TO_DEF TO(DEFAULT)
+#define ZERO_N LT(GAMING_EXTRA, KC_0)
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Level 0: Default Layer
    * ,-------------------------------------------------------------------------------.
@@ -23,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |Esc Fn3| X Gui | C Fn5 | V Alt |<- Fn2 |SPC Fn1| B Alt | N Fn4 | M Gui |Ent Sft|
    * '-------------------------------------------------------------------------------'
    */
-  KEYMAP
+  [DEFAULT] = KEYMAP
      (KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,KC_Y   ,KC_U   ,KC_I   ,KC_O   ,KC_P
      ,A_CTL  ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,KC_H   ,KC_J   ,KC_K   ,KC_L   ,Z_CTL
      ,ESC_F3 ,X_GUI  ,C_F5   ,V_ALT  ,BS_F2  ,SPC_F1 ,B_ALT  ,N_F4   ,M_GUI  ,ENT_SFT
@@ -37,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |   "   |   1   |   2   |   3   |  DEL  |       |   F1  |   F2  |   F3  |  F10  |
    * '-------------------------------------------------------------------------------'
    */
-  KEYMAP
+  [NUM_FUNC] = KEYMAP
      (KC_COMM,KC_7   ,KC_8   ,KC_9   ,KC_0   ,KC_TILD,KC_F7  ,KC_F8  ,KC_F9  ,KC_F12
      ,KC_PLUS,KC_4   ,KC_5   ,KC_6   ,KC_DOT ,KC_GRV ,KC_F4  ,KC_F5  ,KC_F6  ,KC_F11
      ,KC_DQT ,KC_1   ,KC_2   ,KC_3   ,KC_DEL ,KC_TRNS,KC_F1  ,KC_F2  ,KC_F3  ,KC_F10
@@ -51,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |   '   |   !   |   @   |   #   |       |  Tab  |   ?   |   :   |   |   |   ;   |
    * '-------------------------------------------------------------------------------'
    */
-  KEYMAP
+  [SYMBOLS] = KEYMAP
     ( KC_UNDS,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_EQL ,KC_SLSH,KC_LBRC,KC_RBRC,KC_BSLS
     , KC_MINS,KC_DLR, KC_PERC,KC_CIRC,KC_DOT ,KC_TRNS,KC_LT  ,KC_LCBR,KC_RCBR,KC_GT
     , KC_QUOT,KC_EXLM,KC_AT,  KC_HASH,KC_TRNS,KC_TAB ,KC_QUES,KC_COLN,KC_PIPE,KC_SCLN
@@ -65,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |       |       |       |       |       |       |       |       |       |       |
    * '-------------------------------------------------------------------------------'
    */
-  KEYMAP
+  [MOVEMENT] = KEYMAP
     ( KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_MINS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS
     , KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,KC_TRNS
     , KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS
@@ -79,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |       |       |       |       |       |       |
    * '-------------------------------------------------------------------------------'
    */
-  KEYMAP
+  [BIG_MOVE] = KEYMAP
     ( KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_UNDS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS
     , KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_TRNS
     , KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS
@@ -88,15 +101,43 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,-------------------------------------------------------------------------------.
    * | Calc  |  Web  | Mail  |Explore|       |       |       |       |       |       |
    * |-------------------------------------------------------------------------------|
-   * |RGB_TOG|RGB_MOD|RGB_HUI|RGB_HUD|xxxxxxx|xxxxxxx|RGB_SAI|RGB_SAD|RGB_VAI|RGB_VAD|
+   * |RGB_TOG|RGB_MOD|RGB_HUI|RGB_HUD|TO_GAME|xxxxxxx|RGB_SAI|RGB_SAD|RGB_VAI|RGB_VAD|
    * |-------------------------------------------------------------------------------|
    * |       |       |       |       | Flash |       |       |       |       |       |
    * '-------------------------------------------------------------------------------'
    */
-  KEYMAP
+  [RGB] = KEYMAP
     ( KC_CALC,KC_WSCH,KC_MAIL,KC_MYCM,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS
-    , RGB_TOG,RGB_MOD,RGB_HUI,RGB_HUD,KC_NO,  KC_NO,  RGB_SAI,RGB_SAD,RGB_VAI,RGB_VAD
+    , RGB_TOG,RGB_MOD,RGB_HUI,RGB_HUD,TO_GAME,  KC_NO,  RGB_SAI,RGB_SAD,RGB_VAI,RGB_VAD
     , KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,RESET,  KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS
+    ),
+  /* Gaming Layer
+   * ,-------------------------------------------------------------------------------.
+   * |   1   |  Tab  |   Q   |   W   |   E   |   R   |   T   |   7   |   8   |   9   |
+   * |-------------------------------------------------------------------------------|
+   * |   2   |  Ctl  |   A   |   S   |   D   |   F   |   G   |   4   |   5   |   6   |
+   * |-------------------------------------------------------------------------------|
+   * |   3   | Shift |   Z   |   X   |   C   | Space | 0,Num |   1   |   2   |   3   |
+   * '-------------------------------------------------------------------------------'
+   */
+  [GAMING] = KEYMAP
+     (KC_1   ,KC_TAB ,KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,KC_7   ,KC_8   ,KC_9
+     ,KC_2   ,KC_LCTL,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,KC_4   ,KC_5   ,KC_6
+     ,KC_3   ,KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_SPC ,ZERO_N ,KC_1   ,KC_2   ,KC_3
+     ),
+  /* Gaming extra numbers layer
+   * ,-------------------------------------------------------------------------------.
+   * |   4   |       |       |       |       |       |       |       |       |  Esc  |
+   * |-------------------------------------------------------------------------------|
+   * |   5   |       |       |       |       |       |       |       |       |Default|
+   * |-------------------------------------------------------------------------------|
+   * |   6   |       |       |       |       |       |       |       |       |       |
+   * '-------------------------------------------------------------------------------'
+   */
+  [GAMING_EXTRA] = KEYMAP
+    ( KC_4   ,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_ESC
+    , KC_5   ,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,TO_DEF
+    , KC_6   ,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS
     ),
 };
 
